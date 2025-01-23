1a2295f73b4ee4a6e910a1f190c5c74eaf59b362
likhith-b-a <likhithba51@gmail.com>
2025-01-24 02:02:35 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { UserContextProvider } from './Context/UserContext.jsx'
 import { CourseContextProvider } from './Context/CourseContext.jsx'
 
 
-export const server = "https://e17c-2401-4900-6272-6a5d-2da0-9b08-39ea-8425.ngrok-free.app"
+export const server = "https://553d-2401-4900-6272-6a5d-2da0-9b08-39ea-8425.ngrok-free.app"
 
 
 createRoot(document.getElementById('root')).render(
